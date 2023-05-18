1c894a45f799e34fd9d9c87d8fd7c675cd61f5a6
LynovK <90971447+LynovK@users.noreply.github.com>
2023-05-18 08:23:59 +0300

    Update fastest_grad_descent_test.cpp

--- SLAE-main-master/gtest/fastest_grad_descent_test.cpp
+++ SLAE-main-master/gtest/fastest_grad_descent_test.cpp
@@ -4,15 +4,15 @@
 TEST(Gradient, Grad_spusk_test) {
 
     std::vector<element<double>> matrix_CSR = {
-            {0, 0, 10},
-            {0, 1, 2},
-            {0, 2, 1},
-            {1, 0, 2},
-            {1, 1, 10},
-            {1, 2, 3},
-            {2, 0, 1},
-            {2, 1, 3},
-            {2, 2, 10}
+            {3, 3, 0},
+            {2, 5, 2},
+            {1, 4, 1},
+            {3, 5, 2},
+            {2, 1, 10},
+            {12, 2, 3},
+            {0, 0, 1},
+            {6, 1, 3},
+            {5, 1, 10}
     };
     sort_me_plz(matrix_CSR);
     CompressedMatrix<double> res = CompressedMatrix(matrix_CSR, 3, 3);
@@ -35,15 +35,15 @@ TEST(Gradient, Grad_spusk_test) {
 
 TEST(Grad, Gradient_second_test) {
     std::vector<element<double>> matrix_CSR = {
-            {0, 0, 12},
-            {0, 1, 17},
-            {0, 2, 3},
-            {1, 0, 17},
-            {1, 1, 15825},
+            {1, 5, 3},
+            {4, 1, 17},
+            {0, 25, 3},
+            {9, 0, 7},
+            {1, 1, 2},
             {1, 2, 28},
-            {2, 0, 3},
-            {2, 1, 28},
-            {2, 2, 238}
+            {5, 1, 3},
+            {6, 1, 189},
+            {9, 2, 4}
     };
     sort_me_plz(matrix_CSR);
     CompressedMatrix<double> res = CompressedMatrix(matrix_CSR, 3, 3);
