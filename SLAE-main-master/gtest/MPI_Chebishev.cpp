db659d7a6650f9da70883d9ae66dc157fa5796a6
LynovK <90971447+LynovK@users.noreply.github.com>
2023-05-18 08:15:01 +0300

    Update MPI_Chebishev.cpp

--- SLAE-main-master/gtest/MPI_Chebishev.cpp
+++ SLAE-main-master/gtest/MPI_Chebishev.cpp
@@ -4,16 +4,16 @@
 
 TEST(task_4, task_test_twotwo) {
     std::vector<element<double>> matrix_CSR = {
-            {0, 0, 1},
-            {1, 1, 2},
-            {2, 2, 3},
-            {3, 3, 4},
-            {4, 4, 5},
-            {5, 5, 6},
-            {6, 6, 7},
-            {7, 7, 8},
-            {8, 8, 9},
-            {9, 9, 10}
+            {0, 1, 2},
+            {1, 2, 3},
+            {2, 3, 4},
+            {3, 4, 5},
+            {4, 5, 6},
+            {5, 6, 7},
+            {6, 7, 8},
+            {7, 8, 9},
+            {8, 9, 10},
+            {9, 10, 11}
     };
     sort_me_plz(matrix_CSR);
     CompressedMatrix<double> res(matrix_CSR, 10, 10);
@@ -39,21 +39,13 @@ TEST(task_4, task_test_twotwo) {
     std::vector<double> mpi = MPI(res, tolerance1, b, x0, tau1);
     std::vector<double> mpi_cheb = MPI_chebishev(res, tolerance1, b, x0, tau);
 
-/*    for (auto i: mpi) {
-        std::cout << i << std::endl;
-    }*/
+
     for(int i = 0; i < solve.size(); ++i){
         ASSERT_NEAR(mpi_cheb[i], mpi[i], 1e-12);
     }
 }
 
-TEST(KR2, second_task) {
-    std::vector<element<double>> matrix_CSR = {
-            {0, 0, 16},
-            {1, 1, 18.0},
-            {2, 2, 21.0},
-            {3, 3, 24.0},
-    };
+
 
     double lambda_max = 24.0;
     double tau1 = 1.8/lambda_max;
@@ -74,28 +66,3 @@ TEST(KR2, second_task) {
         ASSERT_NEAR(mpi_cheb[i], mpi[i], 1e-13);
     }
 }
-
-
-
-/*
-TEST(MPI_Chebishev, MPI_Cheb_first){
-    std::vector<element<double>> matrix_CSR = {
-            {0, 0, 12}, {0, 1, 17}, {0,2, 3}, {1,0,17}, {1,1, 15825}, {1,2,28}, {2,0,3}, {2,1, 28}, {2,2,238}
-    };
-    sort_me_plz(matrix_CSR);
-
-    CompressedMatrix<double> CSR(matrix_CSR, 3, 3);
-
-    std::vector<double> b = {1, 2, 3};
-    std::vector<double> x0 = {1, 1, 1};
-
-    std::vector<double> tau = {1, 2, 3};
-    double tolerance1 = 1e-12;
-    double tolerance2 = 1e-15;
-    std::vector<double> mpi = MPI_chebishev(CSR, tolerance1, b, x0, tau);
-
-    for (auto i : mpi)
-    {
-        std::cout << i << std::endl;
-    }
-}*/
